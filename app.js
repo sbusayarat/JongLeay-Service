866f81ccf7cf05aee021d13a2dd292a1e3ef43f8
Sata Busayarat <sata.busayarat@hbo.com>
2016-01-17 12:51:43 -0800

    Use vanilla node template instead

---
+++ app.js
@@ -0,0 +1,21 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var router2     = require('./lib/router');
+
+var app        = express();                 // define our app using express
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;        // set our port
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/', router2);
+
+app.listen(port);
+console.info('Server started on port ' + port);
